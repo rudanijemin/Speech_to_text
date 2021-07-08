9b44b6ff6b6b137263b6b9512a9ce035b491b0ef
jemin <rudanijemin18@gmail.com>
2021-07-08 10:30:47 +0530

    speech to text

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: SpeechScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
